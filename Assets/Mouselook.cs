1fb40cda2635ac85a56954053e45ca79a6eddd67
Jack... Jack........JACK <127959212+JackDmearns@users.noreply.github.com>
2026-04-02 12:45:36 +0100

    Looking Acound with mouse looks more flooid and you can look any where

--- Assets/Mouselook.cs
+++ Assets/Mouselook.cs
@@ -3,12 +3,15 @@ using UnityEngine;
 public class Mouselook : MonoBehaviour
 {
     public float mouseSensetivity = 100f;
+    
     public Transform playerBody;
+
+    float xRotation = 0f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
@@ -17,6 +20,10 @@ public class Mouselook : MonoBehaviour
         float mouseX = Input.GetAxis("Mouse X") * mouseSensetivity * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensetivity * Time.deltaTime;
 
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
         playerBody.Rotate(Vector3.up * mouseX);
     }
 }
