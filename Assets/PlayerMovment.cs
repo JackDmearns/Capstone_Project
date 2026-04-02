ba7c82a05caf7f05a7994207a902797a93386967
Jack... Jack........JACK <127959212+JackDmearns@users.noreply.github.com>
2026-04-02 13:58:17 +0100

    Moving Around the map code

--- Assets/PlayerMovment.cs
+++ Assets/PlayerMovment.cs
@@ -2,15 +2,18 @@ using UnityEngine;
 
 public class PlayerMovment : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public CharacterController controller;
+
+    public float speed = 12f;
 
     // Update is called once per frame
     void Update()
     {
-        
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed * Time.deltaTime);
     }
 }
